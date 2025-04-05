07002d954e6b9002c83542534e1c1f0e1cfdfeeb
kouhualin <hualin.kou@gmail.com>
2025-04-05 23:16:38 +0800

    Update base URL for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/lumi_portal2/' : '/',
+  base: '/',
   css: {
     postcss: {
       plugins: [tailwind()],
