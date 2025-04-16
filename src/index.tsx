747c4fe09483c68521e2fa15ed8fe8581d339515
kouhualin <hualin.kou@gmail.com>
2025-04-17 00:04:33 +0800

    fix issues excep waitlist

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Frame } from "./screens/Frame";
+import { MarkdownPage } from "./screens/MarkdownPage";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <Frame />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Frame />} />
+        <Route path="/:type" element={<MarkdownPage />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 );
